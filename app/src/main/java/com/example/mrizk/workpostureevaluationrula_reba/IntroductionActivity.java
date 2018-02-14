028466be40658668c6817f6a443c70f31bfd3445
QIDONK\mrizk_000 <mrizkip@hotmail.com>
2018-02-14 12:41:15 +0700

    first layout, rula and reba, open camera and gallery

--- app/src/main/java/com/example/mrizk/workpostureevaluationrula_reba/IntroductionActivity.java
+++ app/src/main/java/com/example/mrizk/workpostureevaluationrula_reba/IntroductionActivity.java
@@ -3,38 +3,42 @@ package com.example.mrizk.workpostureevaluationrula_reba;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroductionActivity extends AppCompatActivity {
 
-    @BindView(R.id.mainActivity_buttonIntroduction)
-    Button buttonIntroduce;
-    @BindView(R.id.mainActivity_buttonAbout)
-    Button buttonAbout;
+    @BindView(R.id.intro_imageView)
+    ImageView imageViewIntro;
+    @BindView(R.id.intro_buttonReba)
+    Button buttonReba;
+    @BindView(R.id.intro_buttonRula)
+    Button buttonRula;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_introduction);
 
         ButterKnife.bind(this);
 
-        buttonIntroduce.setOnClickListener(new View.OnClickListener() {
+        buttonRula.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, IntroductionActivity.class);
+                Intent intent = new Intent(IntroductionActivity.this, RulaActivity.class);
                 startActivity(intent);
             }
         });
 
-        buttonAbout.setOnClickListener(new View.OnClickListener() {
+        buttonReba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                Intent intent = new Intent(IntroductionActivity.this, RebaActivity.class);
                 startActivity(intent);
             }
         });
