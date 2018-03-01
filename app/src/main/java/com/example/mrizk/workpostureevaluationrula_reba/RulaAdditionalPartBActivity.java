3945d4ee86a96d7e939bce58820ff4bc8d29309b
QIDONK\mrizk_000 <mrizkip@hotmail.com>
2018-03-01 19:11:03 +0700

    update rula and reba additional

---
+++ app/src/main/java/com/example/mrizk/workpostureevaluationrula_reba/RulaAdditionalPartBActivity.java
@@ -0,0 +1,13 @@
+package com.example.mrizk.workpostureevaluationrula_reba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RulaAdditionalPartBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rula_additional_part_b);
+    }
+}
